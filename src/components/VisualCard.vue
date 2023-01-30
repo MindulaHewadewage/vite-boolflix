f9ce097f23233882a0c9ebd3ff82057e63bc777c
MindulaHewadewage <mindufernando@gmail.com>
2023-01-30 11:09:57 +0100

    aggiunta immagini

--- src/components/VisualCard.vue
+++ src/components/VisualCard.vue
@@ -1,17 +1,29 @@
 <script>
+import { imgs } from '../data'
 export default {
     name: 'VisualCard',
     props: {
         visual: Object
+    },
+    computed: {
+        visualImg() {
+            if (!this.visual.poster_path) return imgs.noImage;
+            return imgs.baseUrl + this.visual.poster_path;
+        }
     }
+
 }
 </script>
 
 <template>
     <ul>
         <li>{{ visual.title || visual.name }}</li>
+        <li>``</li>
         <li>{{ visual.original_title || visual.original_name }}</li>
         <li>{{ visual.original_language }}</li>
+        <li>
+            <img :src="visualImg" alt="">
+        </li>
         <li>{{ visual.vote_average }}</li>
     </ul>
 </template>
--- src/data/index.js
+++ src/data/index.js
@@ -3,4 +3,10 @@ const api = {
   baseUri: "https://api.themoviedb.org/3",
 };
 
-export { api };
+const imgs = {
+  baseUrl: "https:image.tmdb.org/t/p/w342",
+  noImage:
+    "https://www.loomcrafts.com/wp-content/uploads/2018/09/placeholder-vertical.jpg",
+};
+
+export { api, imgs };
