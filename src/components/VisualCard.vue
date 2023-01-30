248014fce56d03261d8c2a2451a0c40981f0cf65
MindulaHewadewage <mindufernando@gmail.com>
2023-01-30 12:13:03 +0100

    gestione bandiere ,lingua

--- src/components/VisualCard.vue
+++ src/components/VisualCard.vue
@@ -12,6 +12,14 @@ export default {
         },
         vote() {
             return Math.ceil(this.visual.vote_average / 2)
+        },
+        hasFlag() {
+            const flags = ['it', 'en', 'es', 'fr', 'de', 'pt', 'tr'];
+            return flags.includes(this.visual.original_language);
+        },
+        flagSrc() {
+            const url = new URL(`../assets/img/${this.visual.original_language}.png`, import.meta.url);
+            return url.href;
         }
     }
 
@@ -22,7 +30,10 @@ export default {
     <ul>
         <li>{{ visual.title || visual.name }}</li>
         <li>{{ visual.original_title || visual.original_name }}</li>
-        <li>{{ visual.original_language }}</li>
+        <li>
+            <img v-if="hasFlag" :src="flagSrc" alt="">
+            <div v-else>{{ visual.original_language }}</div>
+        </li>
         <li>
             <img :src="visualImg" alt="">
         </li>
