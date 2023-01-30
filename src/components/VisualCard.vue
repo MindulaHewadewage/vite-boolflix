b574184c0965f418256b9ce4e93c1d1c29959605
MindulaHewadewage <mindufernando@gmail.com>
2023-01-30 15:26:35 +0100

    provando l'hover sulla card

--- src/components/VisualCard.vue
+++ src/components/VisualCard.vue
@@ -28,21 +28,32 @@ export default {
 
 <template>
 
-    <ul class="visual-container">
-        <li>{{ visual.title || visual.name }}</li>
-        <li>{{ visual.original_title || visual.original_name }}</li>
-        <li>
-            <img v-if="hasFlag" :src="flagSrc" class="flag" alt="">
-            <div v-else>{{ visual.original_language }}</div>
-        </li>
-        <li>
-            <!-- <img :src="visualImg" alt=""> -->
-        </li>
-        <li>
-            <i v-for="star in 5" :class="star <= vote ? 'fa-solid' : 'fa-regular'" class="fa-star"></i>
-        </li>
-        <li>{{ visual.overview }}</li>
-    </ul>
+    <div class="visual-container">
+
+        <div class="visual-image">
+            <img :src="visualImg" alt="">
+
+
+            <div class="visual-information">
+                <ul>
+                    <li><b>Title:</b>{{ visual.title || visual.name }}</li>
+                    <li><b>Original Title:</b>{{ visual.original_title || visual.original_name }}</li>
+                    <li>
+                        <b>Original Language:</b>
+                        <img v-if="hasFlag" :src="flagSrc" class="flag" alt="">
+                        <div v-else>{{ visual.original_language }}</div>
+                    </li>
+
+                    <li>
+                        <b>Vote:</b>
+                        <i v-for="star in 5" :class="star <= vote ? 'fa-solid' : 'fa-regular'" class="fa-star"></i>
+                    </li>
+                    <li><b>Release Date:</b>{{ visual.release_date }}</li>
+                    <li><b>Overview:</b>{{ visual.overview }}</li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss">
@@ -50,18 +61,41 @@ export default {
     width: 342px;
     height: 513px;
     border: 1px solid black;
+    background-color: black;
+    color: white;
     border-radius: 10px;
-    padding: 10px;
+    margin: 10px;
     overflow-y: auto;
-
-
 
     display: flex;
     flex-direction: column;
 
+
+    ul {
+        padding: 10px;
+
+        li {
+            padding-top: 5px;
+
+            .flag {
+                width: 30px;
+            }
+
+
+            .fa-star {
+                color: rgb(228, 167, 0);
+            }
+        }
+    }
 }
 
-.flag {
-    width: 30px;
+
+
+.visual-information {
+    display: none;
+}
+
+.visual-image:hover .visual-information {
+    display: block;
 }
 </style>
