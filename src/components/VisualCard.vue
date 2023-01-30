9e33fef1d957a91080ec3b2b87c45cc84addbb76
MindulaHewadewage <mindufernando@gmail.com>
2023-01-30 11:48:45 +0100

    gestione stelline

--- src/components/VisualCard.vue
+++ src/components/VisualCard.vue
@@ -9,6 +9,9 @@ export default {
         visualImg() {
             if (!this.visual.poster_path) return imgs.noImage;
             return imgs.baseUrl + this.visual.poster_path;
+        },
+        vote() {
+            return Math.ceil(this.visual.vote_average / 2)
         }
     }
 
@@ -18,13 +21,14 @@ export default {
 <template>
     <ul>
         <li>{{ visual.title || visual.name }}</li>
-        <li>``</li>
         <li>{{ visual.original_title || visual.original_name }}</li>
         <li>{{ visual.original_language }}</li>
         <li>
             <img :src="visualImg" alt="">
         </li>
-        <li>{{ visual.vote_average }}</li>
+        <li>
+            <i v-for="star in 5" :class="star <= vote ? 'fa-solid' : 'fa-regular'" class="fa-star"></i>
+        </li>
     </ul>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ import { createApp } from "vue";
 import "@fontsource/montserrat";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.vue";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 const app = createApp(App);
-app.component("font-awesome-icon", FontAwesomeIcon);
 app.mount("#app");
